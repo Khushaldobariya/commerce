aaf6d9cd9c2f7826b621cdc3f4fd47839b0937bf
khushaldobariya <khushaldobariya.77@gmail.com>
2022-06-09 09:00:24 -0700

    ecommerce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from 'react-router-dom'
+import Header from "./Component/Header/Header";
+import About from "./Container/About/About";
+import Product from "./Container/Product/Product";
+import Home from "./Container/Home/Home";
+import Service from "./Container/Service/Service";
+import Gallery from "./Container/Gallery/Gallery";
+import Contact from "./Container/Contact/Contact";
+import Footer from "/Component/Footer/Footer";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Switch>
+        <Route exact path={"/"} component={Home} />
+        <Route exact path={"/About"} component={About} />
+        <Route exact path={"/Product"} component={Product} />
+        <Route exact path={"/Service"} component={Service} />
+        <Route exact path={"/Gallery"} component={Gallery} />
+        <Route exact path={"/Contact"} component={Contact} />
+      </Switch>
+      <Footer />
+    </>
+  )
 }
 
 export default App;
